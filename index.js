3e520f5ea2be3adfca75c27251b11cf8803f35ff
leooliveiraa77 <lco.07@hotmail.com>
2022-11-02 13:13:35 -0300

    testing simultaneous clicks

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto('http://cardapio.ufv.br/');
-  //await page.pdf({ path: 'hn.pdf', format: 'a4' });
+const date = new Date();
+const weekDay = date.getDay();
+const day = date.getDate();
+const numberToSunday = 6 - weekDay;
 
-  //await browser.close();
+(async () => {
+  const browser = await puppeteer.launch({ headless: false }); // { headless: false }
+  const page = await browser.newPage();
+  await page.goto('http://cardapio.ufv.br/', {
+    waitUntil: 'networkidle2',
+    timeout: 0,
+  });
+
+  await page.waitForTimeout(5000);
+
+  for (let i = day - 1; i <= day + numberToSunday; i++) {
+    const dayButtons = await page.$$('a.ui-state-default');
+    await dayButtons[i].click();
+    await page.waitForTimeout(1000);
+  }
+
+  await browser.close();
 })();
