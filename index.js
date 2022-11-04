2da0e7e8e9a0f916c33ce9632866608c411e52b6
leooliveiraa77 <lco.07@hotmail.com>
2022-11-04 12:18:27 -0300

    add a function to create a Json with data

--- index.js
+++ index.js
@@ -23,21 +23,25 @@ const numberToSunday = 6 - weekDay;
     await dayButtons[i].click();
     await page.waitForSelector('.titulo_composicao');
 
-    const result = await page.$$eval('.titulo_composicao', (menuDay) => menuDay.map((el) => el.innerText));
+    const result = await page.$$eval('.titulo_composicao i', (menuDay) => menuDay.map((el) => el.innerText));
 
     const weekDayMenu = {
-      lunchMain: result[10],
-      lunchGarrison: result[13],
-      dinnerMain: result[21],
-      dinnerGarrison: result[24],
+      lunchMain: result[8],
+      lunchGarrison: result[11],
+      dinnerMain: result[18],
+      dinnerGarrison: result[21],
     };
 
-    if (result[16] === 'REFRESCO: Uva' || result[16] === 'REFRESCO: Maracujá') {
-      weekDayMenu['juice'] = result[16];
+    if (result[14] === 'Laranja' || result[14] === 'Maracujá') {
+      weekDayMenu['juice'] = result[14];
     }
     console.log(weekDayMenu);
     scrapingData.push(weekDayMenu);
   }
 
+  fs.writeFile('meu-week.json', JSON.stringify(scrapingData, null, 2), (err) => {
+    if (err) throw new Error('something went wrong');
+    console.log('Well done!');
+  });
   await browser.close();
 })();
