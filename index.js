8d50e74ad4985f35c393d8880c0bb02b10ea5e45
leooliveiraa77 <lco.07@hotmail.com>
2022-11-04 10:31:38 -0300

    scraping menu from RU

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 const date = new Date();
 const weekDay = date.getDay();
@@ -6,19 +7,37 @@ const day = date.getDate();
 const numberToSunday = 6 - weekDay;
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false }); // { headless: false }
+  const browser = await puppeteer.launch({ headless: true }); // { headless: false }
   const page = await browser.newPage();
+  //await page.setViewport({ width: 1920, height: 1080 });
   await page.goto('http://cardapio.ufv.br/', {
-    waitUntil: 'networkidle2',
+    waitUntil: 'domcontentloaded',
     timeout: 0,
   });
 
-  await page.waitForTimeout(5000);
+  //await page.waitForTimeout(5000);
+  await page.waitForSelector('.titulo_composicao');
+  const scrapingData = [];
 
   for (let i = day - 1; i <= day + numberToSunday; i++) {
     const dayButtons = await page.$$('a.ui-state-default');
     await dayButtons[i].click();
-    await page.waitForTimeout(1000);
+    await page.waitForSelector('.titulo_composicao');
+
+    const result = await page.$$eval('.titulo_composicao', (menuDay) => menuDay.map((el) => el.innerText));
+
+    const weekDayMenu = {
+      lunchMain: result[10],
+      lunchGarrison: result[13],
+      dinnerMain: result[21],
+      dinnerGarrison: result[24],
+    };
+
+    if (result[16] === 'REFRESCO: Uva' || result[16] === 'REFRESCO: Maracujá') {
+      weekDayMenu['juice'] = result[16];
+    }
+    console.log(weekDayMenu);
+    scrapingData.push(weekDayMenu);
   }
 
   await browser.close();
