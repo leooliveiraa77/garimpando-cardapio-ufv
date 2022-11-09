167ab0efded5e6fad2feb097b3dff6588eacba51
leooliveiraa77 <lco.07@hotmail.com>
2022-11-09 19:41:00 -0300

    adding notion API code in index.js

--- index.js
+++ index.js
@@ -1,16 +1,80 @@
 import { Client } from '@notionhq/client';
 import * as dotenv from 'dotenv';
-
+import { readFile } from 'fs/promises';
 dotenv.config({ path: './.env' });
+
+const jsonData = JSON.parse(await readFile(new URL('./menu-week.json', import.meta.url)));
 
 const notion = new Client({ auth: process.env.NOTION_KEY });
 
-const databaseId = process.env.NOTION_DATABASE_ID;
+//Função para pegar a estrutura e id's do database
+//(Caso tenha duvidas, verifica a documentação do notion API)
 
-(async () => {
-  const blockId = databaseId;
-  const response = await notion.blocks.children.list({
-    block_id: blockId,
+// (async () => {
+//   const databaseId = process.env.NOTION_DATABASE_ID;
+//   const response = await notion.databases.retrieve({ database_id: databaseId });
+//   console.log(response);
+// })();
+
+let classificar = 0;
+
+async function createMenu(menuObj) {
+  notion.pages.create({
+    parent: {
+      database_id: process.env.NOTION_DATABASE_ID,
+    },
+    properties: {
+      title: {
+        title: [
+          {
+            type: 'text',
+            text: {
+              content: menuObj.weekDay,
+            },
+          },
+        ],
+      },
+      [process.env.NOTION_PAGE_ALMOCO]: {
+        rich_text: [
+          {
+            type: 'text',
+            text: {
+              content: `${menuObj.lunchMain} (${menuObj.lunchGarrison})`,
+            },
+          },
+        ],
+      },
+      [process.env.NOTION_PAGE_JANTAR]: {
+        rich_text: [
+          {
+            type: 'text',
+            text: {
+              content: `${menuObj.dinnerMain} (${menuObj.dinnerGarrison})`,
+            },
+          },
+        ],
+      },
+      [process.env.NOTION_PAGE_SUCO]: {
+        rich_text: [
+          {
+            type: 'text',
+            text: {
+              content: menuObj.juice ? menuObj.juice : '',
+            },
+          },
+        ],
+      },
+      [process.env.NOTION_PAGE_CLASSIDICAR]: {
+        number: classificar,
+      },
+    },
   });
-  console.log(response);
-})();
+}
+
+async function sendDataToNotion() {
+  for (const el of jsonData) {
+    await createMenu(el);
+    classificar++;
+  }
+}
+sendDataToNotion();
